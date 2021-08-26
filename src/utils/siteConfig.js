ea1f7ec8e4b449f0900f1dd39cb499daaefe8b50
Vaibhav soni <32386700+white-hat-vaibhs@users.noreply.github.com>
2021-08-25 23:23:06 -0700

    Update siteConfig.js

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: `blog.loveroming.com`, // Site domain. Do not include a trailing slash!
+    siteUrl: `https://blog.loveroming.com`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
